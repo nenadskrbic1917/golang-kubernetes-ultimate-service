2dda5040d47ff776db3411e4aa92ba25ebcab382
nenadskrbic1917 <nenad.skrbic1917@gmail.com>
2026-05-27 20:29:47 +0200

    constructing the logger (initial)

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -1,24 +1,49 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"os"
-	"os/signal"
-	"runtime"
-	"syscall"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var build = "develop"
 
 func main() {
-	g := runtime.GOMAXPROCS(0)
 
-	log.Printf("Starting service build[%s] CPU[%d].", build, g)
-	defer log.Println("Service ended")
+	// Construct the application logger
+	log, err := initLogger("SALES-API")
+	if err != nil {
+		fmt.Printf("error initializing log: %v", err)
+		os.Exit(1)
+	}
+	defer log.Sync()
 
-	shutdown := make(chan os.Signal, 1)
-	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
-	<-shutdown
+	// Perform the startup and shutdown sequence
+	if err := run(log); err != nil {
+		log.Errorw("Startup error", "error", err)
+		os.Exit(1)
+	}
+}
 
-	log.Println("Stopping service")
+func run(log *zap.SugaredLogger) error {
+	return nil
+}
+
+func initLogger(service string) (*zap.SugaredLogger, error) {
+	config := zap.NewProductionConfig()
+	config.OutputPaths = []string{"stdout"}
+	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	config.DisableStacktrace = true
+	config.InitialFields = map[string]interface{}{
+		"service": "SALES-API",
+	}
+
+	log, err := config.Build()
+	if err != nil {
+		return nil, err
+	}
+
+	return log.Sugar(), nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module golang-kubernetes-ultimate-service
 
 go 1.26.0
+
+require go.uber.org/zap v1.28.0
+
+require go.uber.org/multierr v1.10.0 // indirect
